ca77b3e5660f5a532058fd1af27e06c733299508
Sm1t <setp.sm1t@mail.ru>
2017-06-22 23:50:39 +0500

    debug qr codes

--- src/routes/qrs.js
+++ src/routes/qrs.js
@@ -14,10 +14,10 @@ defaultQrs.router.post('/pictures', multipartMiddleware, async(req, res, next) =
 	const img = req.files.picture;
 	try {
 		fs.readFile(img.path, (err, data) => {
-			if (err) next(err);
+			if (err) return res.send(err);
 			const way = path.resolve(__dirname, '../uploads/pictures') + '/' + img.originalFilename;
 			fs.writeFile(way, data, async(err) => {
-				if (err) next(err);
+				if (err) return res.send(err);
 				const link = 'http://arusremservis.ru/qrs/pictures/' + img.originalFilename;
 				await Qr.findOneAndUpdate({_id: req.headers['id']}, {$set: {
 					picture: link
